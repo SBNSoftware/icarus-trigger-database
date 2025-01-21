b67b8cbb03f21d763833e9d70deba41f35750460
Matteo Vicenzi <matteo.vicenzi7@gmail.com>
2025-01-21 10:26:18 -0600

    skip stubs (no run number)

--- select_missing_logs.py
+++ select_missing_logs.py
@@ -7,21 +7,23 @@ from glob import glob
 from tools.parsers import parse_trigger_log, open_trigger_log
 from tools.database_tools import command
 
-source_directory = 
-output_directory = 
+source_directory = "/exp/icarus/data/users/mueller/logs/trigger_logs/"
+output_directory = "/exp/icarus/app/users/mvicenzi/icarus-trigger-database/matching_files/"
 
 def scan_trigger_log(conn, log_directory, no_match_directory):
     
     curs = conn.cursor()
     log_files = glob(f'{log_directory}*')
     for f in log_files:
-        log_name = f.split('/')[-1]
+        log_name = f.split('/')[-1] 
         lines = open_trigger_log(f)
         runs = [x for x in lines if 'Completed the Start transition (Started run) for run ' in x]
+        if(len(runs)<1):
+            print(f'{log_name} has no run number!')
+            continue
         run = runs[0][53:runs[0].find(',')]
         curs.execute('SELECT log_name FROM triggerlog WHERE run_number=%s', (run,))
         res = curs.fetchall()
-        print(res)   
         if len(res) != 0:
             print(f'{log_name} matches run {run} ({res[0]})')
         else:
@@ -33,7 +35,7 @@ def scan_trigger_log(conn, log_directory, no_match_directory):
 def main():
 
     # Load the JSON configuration file
-    configuration_file = "/home/nfs/icarus/triggerDB/config.json"
+    configuration_file = "./config.json"
     config = json.load(open(configuration_file,'r'))
 
     dbconfig = config["database"]
